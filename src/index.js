28aab75af88f44f51bf225cdcf6b45de776c9383
JiYu <146607445+cherrybbang@users.noreply.github.com>
2024-07-30 11:28:01 +0900

    리액트 추가연습

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import App from "./App.js";
 
 const root = createRoot(document.getElementById('root'));
 
+console.log(StrictMode);
+
 // 컴포넌트를 가지고와서 그려주는 파일..
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
 )
+
+// StrictMode
+// 컴포넌트 내 버그 검사
+// 개발전용이며 빌드에는 영향 x
+// 개발 프로세스 초기에 컴포넌트에서 일반적인 버그를 찾는데 도움
